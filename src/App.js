0b5d4fa8309bb2fd207e65a98513962293de47cc
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-08-21 19:11:51 +0200

    tree

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Home from './Components/Home';
+import Tree from './Components/Tree';
 
 function App() {
   return (
     <div className="App">
+      <Tree/>
       <Home />
     </div>
   );
---
+++ src/Components/Tree/index.js
@@ -0,0 +1,10 @@
+import TreeView from '@material-ui/lab/TreeView'
+
+
+function Tree(){
+    return (
+        <div>Tree</div>
+    )
+}
+
+export default Tree
