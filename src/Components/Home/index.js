70b49566d65be089e6268987c49e9c9f0c2ad9e4
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-09-09 11:12:33 +0200

    filter first

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -7,8 +7,9 @@ import Links from '../Links'
 
 function Home() {
 
-    const [data, setData] = useState([])
+    const [data, setData] = useState(null)
     const [selectedNode, setSelectedNode] = useState();
+    // const [filter, setFilter] = useState();
     useEffect(() => {
         const call = async () => {
             let r = await service.getTree();
@@ -18,9 +19,31 @@ function Home() {
         call();
     }, [])
 
+
+    const getFilteredNodes = (nodes, filter) => {
+        let result = [];
+        for (var i = 0; i < nodes.length; i += 1) {
+            if (nodes[i].name.indexOf(filter) > -1) {
+                let tempNode = nodes[i];
+                tempNode.nodes = getFilteredNodes(tempNode.nodes, filter);
+                result.push(tempNode)
+            }
+        }
+        return result;
+    }
+
+    const filterData = (filter) => {
+        debugger;
+        let copyData = { ...data };
+        copyData.nodes = getFilteredNodes(data.nodes, filter);
+        setData(copyData);
+    }
+
     return (
         <div>
             <div>pawel</div>
+            <input onChange={(e) => filterData(e.target.value)}></input>
+            {/* <div>{filter}</div> */}
             <div>selectedNode: {selectedNode && selectedNode.id}</div>
             <div style={{ width: '200px', float: 'left' }}>
                 <Tree structure={data} setSelectedNode={setSelectedNode} selectedNode={selectedNode}></Tree>
