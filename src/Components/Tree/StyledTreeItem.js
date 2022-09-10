635bbf00f324008026129ca1b9618c61d2116854
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-09-10 07:42:52 +0200

    drag and rop treeitem

--- src/Components/Tree/StyledTreeItem.js
+++ src/Components/Tree/StyledTreeItem.js
@@ -8,14 +8,29 @@ function StyledTreeItem({ element, treeLabelClick, ...rest }) {
 
     const [{ isDragging }, drag] = useDrag(() => ({
         type: 'treeItem',
+        item: element,
         collect: monitor => ({
             isDragging: !!monitor.isDragging(),
         }),
     }))
 
+    const [{ isOver }, drop] = useDrop(
+        () => ({
+            accept: 'treeItem',
+            drop: (e) => console.log(e, element),
+            collect: (monitor) => ({
+                isOver: !!monitor.isOver()
+            })
+        }),
+    )
+
     return (
         <TreeItem ref={drag} {...rest} nodeId={element.id.toString()} contextmenuId={element.id}
-            label={<Box><button onClick={(e) => treeLabelClick(e, element.id)}>{element.name}</button><span>{isDragging && '😱'}</span></Box>} >
+            label={<Box ref={drop}>
+                <button onClick={(e) => treeLabelClick(e, element.id)}>{element.name}</button>
+                <span>{isDragging && '😱'}</span>
+                <span> {isOver && <span>Drop Here!</span>}</span>
+            </Box>} >
         </TreeItem >
     )
 }
