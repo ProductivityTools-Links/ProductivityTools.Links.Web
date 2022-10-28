6dac62577b931a3f669c45cdc3eb489a4a0d973f
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-10-28 21:08:00 +0200

    edit

--- src/Components/Links/LinkItem.js
+++ src/Components/Links/LinkItem.js
@@ -1,6 +1,8 @@
 import Paper from '@mui/material/Paper';
 import Tooltip from '@mui/material/Tooltip';
 import { useDrag } from 'react-dnd'
+import EditIcon from '@mui/icons-material/Edit';
+import { IconButton  } from '@mui/material';
 
 
 function LinkItem({ link }) {
@@ -15,10 +17,11 @@ function LinkItem({ link }) {
 
     return (
         <span className="linkItem" ref={drag}>
+
             <span><Tooltip title={link.url}><a href={link.url}>{link.name}</a></Tooltip> </span>
             <span>- {link.description} </span>
             <span>{isDragging && '😱'}</span>
-
+            <button className='editLink'></button>        
         </span>
     )
 }
